be4dfb7857cd274f1176c669ba73437a0713d8f3
Olga Bainova <terra.456@yandex.ru>
2021-11-10 16:17:21 +0500

    feat: create variables and mixins for scss

--- art-quiz/src/index.js
+++ art-quiz/src/index.js
@@ -1,18 +1,20 @@
-import image from './images/lazy.png';
+// import image from './images/lazy.png';
+import './scss/main.scss';
 
-const createImage = (src) => new Promise((res, rej) => {
-  const img = new Image();
-  img.onload = () => res(img);
-  img.onerror = rej;
-  img.src = src;
-});
 
-async function render() {
-  const subHeader = document.createElement('h2');
-  subHeader.innerHTML = 'This elements was created by js';
-  const myImage = await createImage(image);
-  document.body.appendChild(subHeader);
-  document.body.appendChild(myImage);
-}
+// const createImage = (src) => new Promise((res, rej) => {
+//   const img = new Image();
+//   img.onload = () => res(img);
+//   img.onerror = rej;
+//   img.src = src;
+// });
 
-render();
+// async function render() {
+//   const subHeader = document.createElement('h2');
+//   subHeader.innerHTML = 'This elements was created by js';
+//   const myImage = await createImage(image);
+//   document.body.appendChild(subHeader);
+//   document.body.appendChild(myImage);
+// }
+
+// render();
